723125ef9f2a76d9b463593955faecf073466aeb
star4droid <81539602+star4droid@users.noreply.github.com>
2025-01-12 21:36:24 +0200

    Add ./main/java/com/star4droid/star2d/Adapters/EditorValue.java

---
+++ app/src/main/main/java/com/star4droid/star2d/Adapters/EditorValue.java
@@ -0,0 +1,8 @@
+package com.star4droid.star2d.Adapters;
+
+import com.star4droid.star2d.Helpers.PropertySet;
+
+public interface EditorValue {
+	public void setValue(String s);
+	public boolean update(PropertySet p);
+}
