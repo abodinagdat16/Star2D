c031d95e7d7dacfaff23e5bb29ad560ae4fcf126
star4droid <81539602+star4droid@users.noreply.github.com>
2025-01-15 21:25:21 +0200

    Update InternalFutureFailureAccess.java

--- app/src/main/java/com/google/common/util/concurrent/internal/InternalFutureFailureAccess.java
+++ app/src/main/java/com/google/common/util/concurrent/internal/InternalFutureFailureAccess.java
@@ -2,6 +2,7 @@ package com.google.common.util.concurrent.internal;
 
 public abstract class InternalFutureFailureAccess {
     protected InternalFutureFailureAccess() {
+    
     }
 
     protected abstract Throwable tryInternalFastPathGetFailure();
