d7b487f37c5a377047bf5df464490fab6936f8f0
star4droid <81539602+star4droid@users.noreply.github.com>
2025-01-15 21:24:06 +0200

    Delete app/src/main/java/java/com/star4droid/star2d/Adapters/EditorValue.java

--- app/src/main/java/java/com/star4droid/star2d/Adapters/EditorValue.java
+++
@@ -1,8 +0,0 @@
-package com.star4droid.star2d.Adapters;
-
-import com.star4droid.star2d.Helpers.PropertySet;
-
-public interface EditorValue {
-	public void setValue(String s);
-	public boolean update(PropertySet p);
-}
