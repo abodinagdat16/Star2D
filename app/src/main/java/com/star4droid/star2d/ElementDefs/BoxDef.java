5db126c6e9160626345d04798748a67037bee438
Star4Droid <81539602+star4droid@users.noreply.github.com>
2025-01-19 23:55:59 +0200

    Update ./com/star4droid/star2d/ElementDefs/BoxDef.java

--- app/src/main/java/com/star4droid/star2d/ElementDefs/BoxDef.java
+++ app/src/main/java/com/star4droid/star2d/ElementDefs/BoxDef.java
@@ -9,7 +9,7 @@ public class BoxDef {
 	public ElementEvent elementEvents;
 	PropertySet<String,Object> propertySet= new PropertySet<>();
 	public static final String TYPE="BOX";
-	public String image="",type="DYNAMIC",name="", Collision="";
+	public String image="",type="DYNAMIC",name="", Collision="",Script="";
 	public boolean Active=true,Bullet=false,isSensor=false,Fixed_Rotation=false,Visible=true;
 	public float ColliderX=0,x=0,y=0,z=0,rotation=0,ColliderY=0,Gravity_Scale=1,
 	density=1,friction=0,restitution=0.5f,
